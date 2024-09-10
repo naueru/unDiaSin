a0afdd951a63a39786ac292470a878b58f383670
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-10 19:37:52 -0300

    Adds base styles constants

---
+++ constants/styles.ts
@@ -0,0 +1,17 @@
+const COLOR_ALTERNATIVES = {
+  a: {
+    primary200: "#537EC5",
+    primary500: "#293A80",
+    primary800: "#010038",
+    accent500: "#F39422",
+    error500: "#F30A49",
+    white: "#FFFFFF",
+  },
+};
+
+export const GLOBAL_STYLES = {
+  colors: {
+    ...COLOR_ALTERNATIVES.a,
+  },
+  shadow: {},
+};
