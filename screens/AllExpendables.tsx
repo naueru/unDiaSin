78de806c7520a53ac441ae3ec7d7381f2daeb351
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-10 19:30:05 -0300

    Adds base details for expendables list

--- screens/AllExpendables.tsx
+++ screens/AllExpendables.tsx
@@ -1,10 +1,23 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { ExpendablesContext } from "../store/expendables-context";
 
 const AllExpendables: FC = () => {
+  const ExpendablesCtx = useContext(ExpendablesContext);
   return (
     <View style={styles.container}>
       <Text>All expendable poisons</Text>
+      {ExpendablesCtx.expendables.map((expendable) => (
+        <View key={`expendable_${expendable.id}`}>
+          <Text>{expendable.id}</Text>
+          <Text>{expendable.name}</Text>
+          <Text>{expendable.initDay}</Text>
+          <Text>{expendable.initMonth}</Text>
+          <Text>{expendable.initYear}</Text>
+          <Text>{expendable.cost}</Text>
+          <Text>{expendable.timesPerDay}</Text>
+        </View>
+      ))}
     </View>
   );
 };
