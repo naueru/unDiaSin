99e854a86ec2e11532b70c1660f822dd9f00de62
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-11 15:03:36 -0300

    Creates constants file

---
+++ constants/constants.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  expendables: "Expendables",
+  manageExpendable: "ManageExpendable",
+  config: "Config",
+  expendablesOverview: "ExpendablesOverview",
+  expendableDetail: "ExpendableDetail",
+};
