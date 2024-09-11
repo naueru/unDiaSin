ee5a7df4e11497aba005408eed37747ba2bf36d2
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-11 15:28:19 -0300

    Adds navigation after submitting. Sorts imports

--- screens/ManageExpendable.tsx
+++ screens/ManageExpendable.tsx
@@ -1,15 +1,32 @@
+// Core
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { FC, useContext } from "react";
 import { StyleSheet, View } from "react-native";
-import ManageExpendableForm from "../Forms/ManageExpendableForm";
-import { GLOBAL_STYLES } from "../constants/styles";
+
+// Context
 import { ExpendablesContext } from "../store/expendables-context";
+
+// Components
+import ManageExpendableForm from "../Forms/ManageExpendableForm";
+
+// Types
 import { TExpendable } from "../models/Expendables";
 
+// Constants
+import { GLOBAL_STYLES } from "../constants/styles";
+import { ROUTES } from "../constants/constants";
+
 const ManageExpendable: FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
   const expendablesCtx = useContext(ExpendablesContext);
+
   const handleSubmit = (values: TExpendable) => {
     expendablesCtx.addExpendable(values);
+    navigation.navigate(ROUTES.expendables);
   };
+
   return (
     <View style={styles.container}>
       <ManageExpendableForm
