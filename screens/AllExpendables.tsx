7b22e28c23195ea24e48770ae513fce71a204945
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-11 13:03:51 -0300

    Adds icon feedback to check saved date. Adds dummy button to navigate to edit screen

--- screens/AllExpendables.tsx
+++ screens/AllExpendables.tsx
@@ -1,14 +1,24 @@
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { FC, useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { ExpendablesContext } from "../store/expendables-context";
 
 const AllExpendables: FC = () => {
   const ExpendablesCtx = useContext(ExpendablesContext);
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   return (
     <View style={styles.container}>
+      <Button
+        onPress={() => {
+          navigation.navigate("ManageExpendable");
+        }}
+        title="ASD"
+      />
       <Text>All expendable poisons</Text>
       {ExpendablesCtx.expendables.map((expendable) => (
         <View key={`expendable_${expendable.id}`}>
+          <Text>{expendable.icon}</Text>
           <Text>{expendable.id}</Text>
           <Text>{expendable.name}</Text>
           <Text>{expendable.initDay}</Text>
