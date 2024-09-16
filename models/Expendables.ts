171302dd9d414be473f18262f8b353bbe2a34c90
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-16 02:16:12 -0300

    Fixes iconn type

--- models/Expendables.ts
+++ models/Expendables.ts
@@ -1,10 +1,12 @@
+import { TIcons } from "./Icons";
+
 export type TExpendable = {
   id: string;
   name: string;
   initDay: string;
   initMonth: string;
   initYear: string;
-  icon: string;
+  icon: TIcons;
   cost: string;
   timesPerDay: string;
 };
