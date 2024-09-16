7113a55388b8f087ef65552943cb8ee2ea25ec39
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-16 02:24:40 -0300

    Adds date relateds constants

--- constants/constants.ts
+++ constants/constants.ts
@@ -5,3 +5,11 @@ export const ROUTES = {
   expendablesOverview: "ExpendablesOverview",
   expendableDetail: "ExpendableDetail",
 };
+
+export const MILLS_PER_SEC = 1000;
+export const SEC_PER_MIN = 60;
+export const MIN_PER_HOUR = 60;
+export const HOURS_PER_DAY = 24;
+
+export const MILLS_PER_DAY =
+  MILLS_PER_SEC * SEC_PER_MIN * MIN_PER_HOUR * HOURS_PER_DAY; // 86400000
