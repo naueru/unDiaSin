c5d72b5f1ba47f132450b9b74c4fa106fbbff569
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-20 18:32:06 -0300

    Implements themed styles. Adds theme changing dummy button. Sorts imports. Comments imports

--- screens/Config.tsx
+++ screens/Config.tsx
@@ -1,14 +1,37 @@
-import { FC } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
-
+// Core
 import { Ionicons } from "@expo/vector-icons";
+import { FC } from "react";
+import { Appearance, Button, FlatList, Text, View } from "react-native";
+
+// Hooks
+import { useColorTheme } from "../hooks/styles";
+
+// Utils
+import { createThemedStyle } from "../utils/styles";
+
+// Constants
 import { ICONS_SORTED } from "../constants/IconsSorted";
 import { GLOBAL_STYLES } from "../constants/styles";
 
 const Config: FC = () => {
+  const scheme = useColorTheme();
+  const styles = computedStyles[scheme];
   return (
     <View style={styles.container}>
-      <Text>Config</Text>
+      <Text style={styles.title}>Config</Text>
+      <View style={styles.theme}>
+        <Text style={styles.title}>{scheme}</Text>
+        <View style={styles.themeButtons}>
+          <Button
+            onPress={() => Appearance.setColorScheme("dark")}
+            title="dark"
+          />
+          <Button
+            onPress={() => Appearance.setColorScheme("light")}
+            title="light"
+          />
+        </View>
+      </View>
       <FlatList
         data={ICONS_SORTED.filter(
           (item: string) =>
@@ -19,7 +42,7 @@ const Config: FC = () => {
             <Ionicons
               name={item}
               size={30}
-              color={GLOBAL_STYLES.colors.accent500}
+              color={GLOBAL_STYLES.colors[scheme].accent500}
             />
             <Text style={styles.text}>{item}</Text>
           </View>
@@ -33,9 +56,9 @@ const Config: FC = () => {
 
 export default Config;
 
-const styles = StyleSheet.create({
+const computedStyles = createThemedStyle({
   container: {
-    backgroundColor: GLOBAL_STYLES.colors.primary500,
+    backgroundColor: "primary500",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
@@ -48,6 +71,20 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 8,
     textAlign: "center",
-    color: GLOBAL_STYLES.colors.accent500,
+    color: "accent500",
+  },
+  title: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "accent500",
+  },
+  theme: {
+    gap: 16,
+    padding: 32,
+  },
+  themeButtons: {
+    flexDirection: "row",
+    gap: 16,
+    width: "100%",
   },
 });
