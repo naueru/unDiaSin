e97b2170036c0f3f533d0a7b85e39727127dce39
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-20 18:38:12 -0300

    Implements color types. Adds secondary color. Removes spreaded colors

--- constants/styles.ts
+++ constants/styles.ts
@@ -1,17 +1,30 @@
-const COLOR_ALTERNATIVES = {
-  a: {
+import { IColorVariants, IGlobalStyles } from "../models/styles";
+
+const COLOR_VARIANTS: IColorVariants = {
+  dark: {
     primary200: "#537EC5",
     primary500: "#293A80",
     primary800: "#010038",
+    secondary800: "#FFFFFF",
     accent500: "#F39422",
+    error500: "#F30A49",
+    white: "#FFFFFF",
+  },
+  light: {
+    primary200: "#FCF7F8",
+    primary500: "#CED3DC",
+    primary800: "#ABA9C3",
+    secondary800: "#000000",
+    accent500: "#275DAD",
     error500: "#F30A49",
     white: "#FFFFFF",
   },
 };
 
-export const GLOBAL_STYLES = {
+export const GLOBAL_STYLES: IGlobalStyles = {
   colors: {
-    ...COLOR_ALTERNATIVES.a,
+    ...COLOR_VARIANTS.dark,
+    ...COLOR_VARIANTS,
   },
   shadow: {
     elevation: 8,
