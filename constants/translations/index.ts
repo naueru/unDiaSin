532d57b4c0c242ea7e3e050a686c013f0ae0e8c3
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-25 03:33:27 -0300

    Creates module exports for translations files

---
+++ constants/translations/index.ts
@@ -0,0 +1,12 @@
+import { TTranslations } from "../../models/translations";
+import { spanish } from "./spanish";
+import { english } from "./english";
+import { japanese } from "./japansese";
+
+const TRANSLATIONS: TTranslations = {
+  spanish,
+  english,
+  japanese,
+};
+
+export default TRANSLATIONS;
