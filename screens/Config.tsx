36a745ab694bf53cf74c12ce931e544dfb3a81c2
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-25 04:02:29 -0300

    Implements translations. Adds modal for icons list to clean the screen.

--- screens/Config.tsx
+++ screens/Config.tsx
@@ -1,56 +1,81 @@
 // Core
+import { BottomSheetFlatList, BottomSheetModal } from "@gorhom/bottom-sheet";
 import { Ionicons } from "@expo/vector-icons";
-import { FC } from "react";
-import { Appearance, Button, FlatList, Text, View } from "react-native";
+import { Appearance, Text, View } from "react-native";
+import { FC, useContext, useRef } from "react";
 
 // Hooks
 import { useColorTheme } from "../hooks/styles";
 
+// Components
+import PressableIcon from "../components/PressableIcon";
+import ThemeToggle from "../components/ThemeToggle";
+import LanguageSelect from "../components/LanguageSelect";
+import BSModal from "../components/BSModal";
+import Title from "../components/Title";
+
+// Context
+import { TranslationsContext } from "../store/language-context";
+
 // Utils
 import { createThemedStyle } from "../utils/styles";
+
+// Types
+import { TIcons } from "../models/Icons";
 
 // Constants
 import { ICONS_SORTED } from "../constants/IconsSorted";
 import { GLOBAL_STYLES } from "../constants/styles";
-import ThemeToggle from "../components/ThemeToggle";
-import Title from "../components/Title";
 
 const Config: FC = () => {
+  const { translation, chooseLanguage, language } =
+    useContext(TranslationsContext);
   const scheme = useColorTheme();
   const styles = computedStyles[scheme];
   const isDark = scheme === "dark";
+  const sheetRef = useRef<BottomSheetModal>(null);
   return (
     <View style={styles.container}>
-      <Title label="Tema" />
-      <View style={styles.theme}>
-        <View style={styles.themeButtons}>
-          <ThemeToggle
-            onChange={(value: boolean) =>
-              Appearance.setColorScheme(value ? "dark" : "light")
-            }
-            value={isDark}
-          />
-        </View>
-      </View>
-      <Title label="Idioma" />
-      <FlatList
-        data={ICONS_SORTED.filter(
-          (item: string) =>
-            item.indexOf("sharp") === -1 && item.indexOf("outline") === -1
-        )}
-        renderItem={({ item }) => (
-          <View style={styles.iconContainer}>
-            <Ionicons
-              name={item}
-              size={30}
-              color={GLOBAL_STYLES.colors[scheme].accent500}
-            />
-            <Text style={styles.text}>{item}</Text>
-          </View>
-        )}
-        keyExtractor={(item) => "icon_" + item}
-        numColumns={8}
+      <Title label={translation.CONFIG_THEME_TITLE} />
+
+      <ThemeToggle
+        onChange={(value: boolean) =>
+          Appearance.setColorScheme(value ? "dark" : "light")
+        }
+        value={isDark}
       />
+
+      <Title label={translation.CONFIG_LANG_TITLE} />
+      <LanguageSelect
+        defaultValue={language}
+        onSelect={(value: string) => chooseLanguage(value)}
+      />
+      <Title label={translation.CONFIG_ICONS_TITLE} />
+      <PressableIcon
+        name="images"
+        onPress={() => sheetRef.current?.present()}
+        size={60}
+      />
+      <BSModal ref={sheetRef}>
+        <BottomSheetFlatList
+          data={ICONS_SORTED.filter(
+            (item: TIcons) =>
+              item.indexOf("sharp") === -1 && item.indexOf("outline") === -1
+          )}
+          renderItem={({ item }: { item: TIcons }) => (
+            <View style={styles.iconContainer}>
+              <Ionicons
+                name={item}
+                size={30}
+                color={GLOBAL_STYLES.colors[scheme].accent500}
+              />
+              <Text style={styles.text}>{item}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => "icon_conf_" + item}
+          numColumns={8}
+        />
+      </BSModal>
     </View>
   );
 };
@@ -62,6 +87,7 @@ const computedStyles = createThemedStyle({
     alignItems: "center",
     backgroundColor: "primary500",
     flex: 1,
+    gap: 32,
     justifyContent: "center",
     padding: 32,
   },
@@ -74,19 +100,5 @@ const computedStyles = createThemedStyle({
     fontSize: 8,
     textAlign: "center",
     color: "accent500",
-  },
-  title: {
-    fontSize: 16,
-    textAlign: "center",
-    color: "accent500",
-  },
-  theme: {
-    gap: 16,
-    padding: 32,
-  },
-  themeButtons: {
-    flexDirection: "row",
-    gap: 16,
-    width: "100%",
   },
 });
