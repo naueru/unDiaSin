72eb9c8f75b54f7d59239033ea0215ae1a051a6d
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-25 14:46:37 -0300

    Implements device storage

--- store/language-context.tsx
+++ store/language-context.tsx
@@ -1,4 +1,5 @@
 // Core
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 import { createContext, PropsWithChildren, useState } from "react";
 
 // Types
@@ -7,6 +8,7 @@ import { TTranslation, TTranslationsKeys } from "../models/translations";
 // Constants
 import TRANSLATIONS from "../constants/translations";
 import { DEFAULT_LANGUAGE } from "../constants/defaults";
+import { STORAGE_KEY_LANGUAGE } from "../constants/constants";
 
 export interface ITranslationContext {
   language: string;
@@ -22,9 +24,11 @@ export const TranslationsContext = createContext<ITranslationContext>({
 
 const TranslationsContextProvider = ({ children }: PropsWithChildren) => {
   const [language, setLanguage] = useState<TTranslationsKeys>(DEFAULT_LANGUAGE);
+  const asyncStorage = useAsyncStorage(STORAGE_KEY_LANGUAGE);
 
   const chooseLanguage = (language: TTranslationsKeys) => {
     setLanguage(() => language);
+    asyncStorage.setItem(language);
   };
 
   const value = {
