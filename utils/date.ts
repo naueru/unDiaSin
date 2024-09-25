63c5b38223adbd06996c2e055d6e8256ba003fad
Nahuel Andrada <nahuel@euroasiapay.com>
2024-09-25 03:59:03 -0300

    Adds gerMaxDaysInMonth utility function

--- utils/date.ts
+++ utils/date.ts
@@ -1,6 +1,13 @@
-import { MILLS_PER_DAY } from "../constants/constants";
+// Constants
+import { DAYS_PER_MONTH, MILLS_PER_DAY } from "../constants/constants";
 
 export const getDaysDiff = (endDate: Date, startDate: Date): number => {
   const diff = endDate.getTime() - startDate.getTime();
   return Math.floor(diff / MILLS_PER_DAY);
+};
+
+export const getMaxDaysInMonth = (month: number, year: number) => {
+  let days = DAYS_PER_MONTH[month - 1];
+  if (month === 2 && year % 4 === 0) days++;
+  return days;
 };
